1408c9ca626130b1b8f4281cac9289dc79a6f306
calmdowngirl <116081897+calmdowngirl@users.noreply.github.com>
2025-04-06 15:17:31 +1000

    fix colour

--- routes/index.tsx
+++ routes/index.tsx
@@ -64,19 +64,25 @@ export default define.page<typeof handler>(({ data }) => {
             height="128"
             alt="the Fresh logo: a sliced lemon dripping with juice"
           />
-          <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+          <h1 class="text-4xl font-bold">welcom, sloth</h1>
           <div class="my-4">
             <Partial name={data.isAuthor ? "action-author" : "action-login"}>
               {data.isAuthor
                 ? (
                   <span>
                     <a
-                      class="text-black underline visited:text-black g-blue-500 active:bg-blue-600"
+                      class="fresh-gradient text-purple-600 underline hover:no-underline visited:text-purple-600"
                       href="/add/log"
                     >
                       log smtg
                     </a>{" "}
-                    or <a class="underline" href="/add/say">say smtg</a>
+                    or{" "}
+                    <a
+                      class="fresh-gradient text-purple-600 underline hover:no-underline visited:text-purple-600"
+                      href="/add/say"
+                    >
+                      say smtg
+                    </a>
                   </span>
                 )
                 : (
